24fbf06f0c1638a1ef23d69571c77f768ad1a9ff
klsoundarya <k.soundarya7@gmail.com>
2024-12-12 20:13:00 +0000

    style & improve search bar funct and submission handling

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,26 @@
-// 
+document.addEventListener('DOMContentLoaded', function () {
+    const searchTrigger = document.getElementById('searchButton');
+    const searchInput = document.getElementById('searchInput');
+    const closeSearch = document.getElementById('closeSearch');
+
+    // Trigger search input expansion
+    searchTrigger.addEventListener('click', function (e) {
+        if (!searchInput.classList.contains('show')) {
+            e.preventDefault(); // Prevent form submission on expand
+            searchInput.classList.add('show');
+            closeSearch.classList.add('show');
+        }
+    });
+
+    // Close search input
+    closeSearch.addEventListener('click', function () {
+        searchInput.classList.remove('show');
+        closeSearch.classList.remove('show');
+    });
+
+    searchInput.addEventListener('keydown', function (e) {
+        if (e.key === 'Enter') {
+            document.querySelector('.search-bar').submit();
+        }
+    });
+});
