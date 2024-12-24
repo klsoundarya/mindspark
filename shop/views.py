3923561a0533cc7b7cad84a1fa2413a8c4056770
klsoundarya <k.soundarya7@gmail.com>
2024-12-24 19:34:42 +0000

    add product images to the media

--- shop/views.py
+++ shop/views.py
@@ -10,6 +10,11 @@ def all_essentials(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    for product in products:
+        if product.rating:
+            product.display_rating = round(product.rating * 2) / 2
+        else:
+            product.display_rating = None
 
     context = {
         'products': products,
