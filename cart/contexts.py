16ff7a8d7ccb82d90e6b72690433734509188bad
klsoundarya <k.soundarya7@gmail.com>
2025-01-03 18:38:40 +0000

    add the add to cart functionality

--- cart/contexts.py
+++ cart/contexts.py
@@ -2,7 +2,6 @@ from decimal import Decimal
 from django.conf import settings
 
 def cart_contents(request):
-
     cart_items = []
     total = 0
     product_count = 0
@@ -13,9 +12,9 @@ def cart_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
-    
+
     context = {
         'cart_items': cart_items,
         'total': total,
@@ -25,5 +24,5 @@ def cart_contents(request):
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
-
+    
     return context
--- cart/urls.py
+++ cart/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.cart_summary, name="cart_summary"),
+    path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
 ]
