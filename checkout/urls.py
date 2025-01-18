ade3f9096d61c5c12e241e8ffe1f2534d3e5fafa
klsoundarya <k.soundarya7@gmail.com>
2025-01-18 17:12:06 +0000

    add checkout form data caching in payment intent

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('order/<int:order_id>/', views.order_details, name='order_details'),
     path('wh/', webhook, name='webhook'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
 ]
