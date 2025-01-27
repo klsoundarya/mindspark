cb1d880f2baebf809dbb37aa0c9b8bb6b7cde3a1
klsoundarya <k.soundarya7@gmail.com>
2025-01-27 14:40:02 +0000

    add & update order history and checkout views

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('password_update/', views.password_update_view, name='password_update'),
     path('profiles/delete/', views.delete_account, name='delete_account'),
+    path('order_history/<order_number>', views.order_history, name='order_history'),
     path('', views.profile, name='profile')
 ]
