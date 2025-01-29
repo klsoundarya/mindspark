f5ad98fc473d1afbfceec612981a26174419f68b
klsoundarya <k.soundarya7@gmail.com>
2025-01-29 01:59:49 +0000

    fix the image field part-2 for shop admin

--- shop/widgets.py
+++ shop/widgets.py
@@ -1,5 +1,6 @@
 from django.forms.widgets import ClearableFileInput
 from django.utils.translation import gettext_lazy as _
+from django.utils.safestring import mark_safe
 
 
 class CustomClearableFileInput(ClearableFileInput):
