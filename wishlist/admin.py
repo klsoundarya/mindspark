1fea71a98de0b8aa6d2d0a52eeea2162ce5e1f1b
klsoundarya <k.soundarya7@gmail.com>
2025-02-10 21:47:20 +0000

    Implement wishlist views, and templates to allow users to add and remove products from their wishlist.

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from .models import Wishlist
+
+@admin.register(Wishlist)
+class AdminWishlist(admin.ModelAdmin):
+    """Wishlist admin panel configuration"""
+    model = Wishlist
+    fields = ('user', 'product')
+    list_display = ('pk', 'user', 'product', 'added_at')
+    list_filter = ('user', 'added_at')
+    search_fields = ('user__username', 'product__name')
+    ordering = ('-added_at',)
+    readonly_fields = ('added_at',)
