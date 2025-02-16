98550854cb4bf499ecdcdbf08948c6c4b315656b
klsoundarya <k.soundarya7@gmail.com>
2025-02-16 16:34:32 +0000

    Add blog highlights section to home page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from testimonials.models import Testimonial
+from read.models import Article
 
 def home_view(request):
     """
-    Renders the Home page
+    Renders the Home page with latest articles and testimonials.
     """
+    latest_articles = Article.objects.filter(published=True).order_by('-created_at')[:3]
     testimonials = Testimonial.objects.all().order_by('-created_at')[:4]
-    return render(request, 'home/home.html', {'testimonials': testimonials})
+    
+    return render(request, 'home/home.html', {
+        'latest_articles': latest_articles,
+        'testimonials': testimonials
+    })
--- read/urls.py
+++ read/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/', views.article_list, name='article_list'),
-    path('blog/<int:pk>/', views.article_detail, name='article_detail'),
-    path('blog/new/', views.article_create, name='article_create'),  # Superuser only
-    path('publish_article/<int:pk>/', views.publish_article, name='publish_article'),
-    path('delete_article/<int:pk>/', views.delete_article, name='delete_article'), 
-    path('article/<int:pk>/edit/', views.edit_article, name='edit_article'),
+    path('', views.article_list, name='article_list'),
+    path('read/<int:pk>/', views.article_detail, name='article_detail'),
+    path('read/new/', views.article_create, name='article_create'),  # Superuser only
+    path('read/<int:pk>/publish/', views.publish_article, name='publish_article'),
+    path('read/<int:pk>/delete/', views.delete_article, name='delete_article'),
+    path('read/<int:pk>/edit/', views.edit_article, name='edit_article'),
 ]
