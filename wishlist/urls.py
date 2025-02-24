0d10a335df115a8aea01a52d36adae0d0007fe92
klsoundarya <k.soundarya7@gmail.com>
2025-02-24 18:43:00 +0000

    Fix bug where card number was not displaying in checkout app

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import add_to_wishlist, remove_from_wishlist, wishlist_view, admin_wishlist_view
 
 urlpatterns = [
-    path("wishlist/", wishlist_view, name="wishlist"),
+    path("", wishlist_view, name="wishlist"),
     path("wishlist/add/<int:product_id>/", add_to_wishlist, name="add_to_wishlist"),
     path("wishlist/remove/<int:product_id>/", remove_from_wishlist, name="remove_from_wishlist"),
     path('admin/wishlists/', admin_wishlist_view, name='admin_wishlist_view'),
